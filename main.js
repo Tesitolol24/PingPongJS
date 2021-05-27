3f66d8c692afb84f1d98c4640c84b8668743aeac
Juan Pablo Mesa <tesitolol24@gmail.com>
2021-05-27 14:22:30 -0500

    Se creo un board el cual mostramos por pantalla en el index

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
-function main(){
+(function(){
+    self.Board = function(width, height){
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.gameOver = false;
+        this.bars = [];
+        this.ball = null;
+    }   
 
+    self.Board.prototype = {
+        get elements(){
+            var elements = this.bars;
+            elements.push(ball);
+        }
+    }
+})();
+
+(function(){
+    self.BoardView = function(canvas,board){
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+    }
+})();
+
+window.addEventListener("load",main);
+
+function main(){
+    var board = new Board(800,400);
+    console.log(board);
+    var canvas = document.getElementById('canvas');
+    var board_view = new BoardView(canvas,board);
 }
