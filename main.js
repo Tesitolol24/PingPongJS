4b20e161dd38b99e8c3fcaa90d896db4450eeed5
Juan Pablo Mesa <tesitolol24@gmail.com>
2021-05-28 07:19:17 -0500

    Se creo la funcion draw para imprimir barras y persiste el error lenght

--- main.js
+++ main.js
@@ -11,7 +11,28 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
+        }
+    }
+})();
+
+(function(){
+    self.Bar = function(x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+
+        self.Bar.prototype = {
+            dawn: function(){
+
+            },
+            up: function(){
+                
+            }
         }
     }
 })();
@@ -23,6 +44,25 @@
         this.canvas.height = board.height;
         this.board = board;
         this.ctx = canvas.getContext("2d");
+    },
+
+    self.BoardView.prototype = {
+        draw: function(){ 
+            for (var i = this.board.elements.lenght -1; i >= 0; i--) {
+                var el = this.board.elements[index];
+                draw(this.ctx,el);
+            };
+        }
+    },
+
+    function draw(ctx,element){
+        if(element != null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
     }
 })();
 
@@ -30,7 +70,8 @@ window.addEventListener("load",main);
 
 function main(){
     var board = new Board(800,400);
-    console.log(board);
+    var bar = new Bar(20,100,40,100,board);
     var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas,board);
+    var board_view = new BoardView(canvas,board);  
+    board_view.draw();
 }
